0a48a90f6294f699cf9efe68d10ff002cc42823c
sukigsx <sukigsx.mbsistemas@gmail.com>
2022-01-22 10:30:26 +0100

    Update intrusos.sukigsx.sh

--- codigo/intrusos.sukigsx.sh
+++ codigo/intrusos.sukigsx.sh
@@ -92,3 +92,91 @@ else
         echo -e " ${amarillo}Imposible la comprobacion de actualizaciones del script.${borra_colores}"
         echo ""
 fi
+
+
+#maximiza la terminal.
+wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
+
+while :
+do
+clear
+#maximiza la terminal.
+wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
+echo -e "${rosa}"; figlet -c sukigsx; echo -e "${borra_colores}"
+echo ""
+echo -e "${verde} Diseñado por sukigsx / Contacto:   sukigsx.mbsistemas@gmail.com${borra_colores}"
+echo -e "${verde}                                    https://mbsistemas.ddns.net${borra_colores}"
+echo ""
+echo -e "${verde} Nombre del script < $0 > Comprueba tu red lan/wifi de Intrusos.  ${borra_colores}"
+echo ""
+echo -e "     0. ${azul}Actualiza este scripts.${borra_colores}"
+echo -e ""
+echo -e "     1. ${azul}Actualizacion de sistema con update.${borra_colores}"
+echo -e "     2. ${azul}Actualizacion de sistema con upgrade.${borra_colores}"
+echo -e "     3. ${azul}Actualizar sistema completo con update y upgrade.${borra_colores}"
+echo -e "     4. ${azul}Actualizar completo. ( Solo valido para kde ).${borra_colores}"
+echo -e "     5. ${azul}Instalar repositorios y actualizar.${borra_colores}"
+echo ""
+echo -e "     6. ${azul}Instalar software de repositorios.${borra_colores}"
+echo -e "     7. ${azul}Instalar software externo.${borra_colores}"
+echo ""
+echo -e "     8. ${azul}Buscar equipos en la red.${borra_colores}"
+echo -e "     9. ${azul}Conectar a equipo de red.${borra_colores}"
+echo ""
+echo -e "    40. ${azul}Instalar scripts de sukigsx.${borra_colores}"
+echo ""
+echo -e "    90. ${azul}Ayuda.${borra_colores}"
+echo -e "    99. ${azul}Salir.${borra_colores}"
+echo ""
+echo -n " Seleccione una opcion del menu --->>> "
+read opcion
+case $opcion in
+    
+    0) #actualiza el script
+        if [ -e /usr/bin/$0 ] #comprueba si se ha instalado el script con el deb, comprobando el fichero /usr/bin/inicio.sukigsx.sh
+        then
+            ruta="/usr/bin"
+            cd /tmp
+            mkdir temporal_update
+            git clone $repositorio /tmp/temporal_update
+            cd /tmp/temporal_update/codigo/
+            sudo chmod +x $ruta/*.sukigsx.sh
+            sudo cp -r /tmp/temporal_update/codigo/* $ruta
+            sudo rm -r /tmp/temporal_update
+            clear
+            echo "";
+            echo -e "${verde} Script actualizado. Tienes que reiniciar el script para ver los cambios.${borra_colores}";
+            echo "";
+            read -p " Pulsa una tecla para continuar." pause
+            ctrl_c;
+        else
+            ruta=$(pwd)
+            cd /tmp
+            mkdir temporal_update
+            git clone $repositorio /tmp/temporal_update
+            cd /tmp/temporal_update/codigo/
+            sudo chmod +x $ruta/*.sukigsx.sh
+            sudo cp -r /tmp/temporal_update/codigo/* $ruta
+            sudo rm -r /tmp/temporal_update
+            clear
+            echo "";
+            echo -e "${verde} Script actualizado. Tienes que reiniciar el script para ver los cambios.${borra_colores}";
+            echo "";
+            read -p " Pulsa una tecla para continuar." pause
+            ctrl_c;
+        fi;; 
+        
+    90) #ayuda
+        ayuda;;
+                
+    99) #salida
+        ctrl_c;;
+
+    *)  #se activa cuando se introduce una opcion no controlada del menu
+        echo "";
+        echo -e " ${amarillo}OPCION NO DISPONIBLE EN EL MENU.    Seleccion 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 40, 90 o 99 ${borra_colores}";
+        echo -e " ${amarillo}PRESIONA ENTER PARA CONTINUAR ........${borra_colores}";
+        echo "";
+        read pause;;
+esac
+done
